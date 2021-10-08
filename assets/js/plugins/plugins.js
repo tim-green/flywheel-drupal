4611c8d26a6bcac46cdbcc2051045fac80262e5d
Tim <github@timgreen.ws>
2021-10-08 18:46:51 +1100

    added smartmenu script for theme

--- assets/js/plugins/plugins.js
+++ assets/js/plugins/plugins.js
@@ -1,3 +1,26 @@
+
+function flywheelMenu(){
+
+	// Main menu
+	jQuery('#main-menu').smartmenus();
+	
+	// Mobile menu toggle
+	jQuery('.navbar-toggle').click(function(){
+		jQuery('.region-primary-menu').addClass('expand');
+	});
+	jQuery('.navbar-toggle-close').click(function(){
+		jQuery('.region-primary-menu').removeClass('expand');
+	});
+
+	// Mobile dropdown menu
+	if ( jQuery(window).width() < 767) {
+		jQuery(".region-primary-menu li a:not(.has-submenu)").click(function () {
+			jQuery('.region-primary-menu').hide();
+	    });
+	}
+
+}
+
 function homeSlider(){
 	jQuery('.flexslider').flexslider({
     	animation: "slide"	
@@ -5,5 +28,6 @@ function homeSlider(){
 }
 
 jQuery(document).ready(function($){
+	flywheelMenu();
 	homeSlider();
 });
